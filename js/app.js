09d10f4c1b537d52fd1352417360038300d52555
Anamarija <anamarija.jukic97@gmail.com>
2020-07-03 15:40:48 +0200

    Typing effect

--- js/app.js
+++ js/app.js
@@ -9,3 +9,30 @@ scrollToTheTopBtn.addEventListener("click", function() {
     behavior: "smooth"
   });
 });
+
+
+
+// typing effect
+
+const texts = ["ready to move", " need cleaning", "need landscaping" ];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+  if (count === texts.length){
+    count = 0;
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent=letter;
+  if(letter.length === currentText.length){
+    count++;
+    index=0;
+  }
+  setTimeout(type, 150);
+} ());
