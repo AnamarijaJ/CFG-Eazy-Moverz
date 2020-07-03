94678a1564bde4f37a93d34f16ff842dfa8d7c81
Anamarija <anamarija.jukic97@gmail.com>
2020-07-03 12:16:01 +0200

    Scroll to the top button

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// scroll to the top button
+
+const scrollToTheTopBtn = document.getElementById("back-to-the-top-btn");
+
+scrollToTheTopBtn.addEventListener("click", function() {
+  window.scrollTo(0, 0);
+});
